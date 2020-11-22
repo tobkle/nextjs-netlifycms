964926506d317c088101ff6b2bc4da01cf74ea5d
Tobias Klemmer <tobias@klemmer.info>
2020-11-22 09:23:45 +0100

    V0

--- cms/config.js
+++ cms/config.js
@@ -1,40 +1,40 @@
 export default {
-  cms_manual_init: true,
-  backend: {
-    name: 'github',
-    repo: 'jakeprins/nextjs-netlify-cms',
-    branch: 'main',
-  },
-  media_folder: 'public/img',
-  public_folder: 'img',
-  collections: [
-    {
-      name: 'pages',
-      label: 'Pages',
-      files: [
-        {
-          label: 'Home',
-          name: 'home',
-          file: 'content/pages/home.md',
-          fields: [
-            {
-              label: 'Hero Title',
-              name: 'hero_title',
-              widget: 'string',
-            },
-            {
-              label: 'Hero Description',
-              name: 'hero_description',
-              widget: 'markdown',
-            },
-            {
-              label: 'Hero Image',
-              name: 'hero_image',
-              widget: 'image',
-            },
-          ],
-        },
-      ],
+    cms_manual_init: true,
+    backend: {
+        name: 'github',
+        repo: 'tobkle/nextjs-netlifycms-example',
+        branch: 'main',
     },
-  ],
-};
+    media_folder: 'public/img',
+    public_folder: 'img',
+    collections: [
+        {
+            name: 'pages',
+            label: 'Pages',
+            files: [
+                {
+                    label: 'Home',
+                    name: 'home',
+                    file: 'content/pages/home.md',
+                    fields: [
+                        {
+                            label: 'Hero Title',
+                            name: 'hero_title',
+                            widget: 'string',
+                        },
+                        {
+                            label: 'Hero Description',
+                            name: 'hero_description',
+                            widget: 'markdown',
+                        },
+                        {
+                            label: 'Hero Image',
+                            name: 'hero_image',
+                            widget: 'image',
+                        },
+                    ],
+                },
+            ],
+        },
+    ],
+}
